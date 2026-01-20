034adb340dbd8cac61cdd7f43638470bf5fb4703
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-01-20 03:49:42 +0000

    gg

--- WDLesson3_5Practical_Soln/script.js
+++ WDLesson3_5Practical_Soln/script.js
@@ -4,11 +4,19 @@
       3) Display the results in the appropriate element
 */
 function recArea(){
-
+      let l = parseFloat( document.getElementById("len").value);
+      let w = parseFloat( document.getElementById("wid").value);
+      let op = document.getElementById("output");
+      let A = l * w;
+      op.innerHTML = "Area of rectangle is: " + A;
 }
 
 function recPerimeter(){
-
+      let l = parseFloat( document.getElementById("len").value);
+      let w = parseFloat( document.getElementById("wid").value);
+      let op = document.getElementById("output");
+      let P = 2 *(l+w);
+      op.innerHTML = "Perimeter of rectangle is: " + P;
 }
 
 
@@ -20,11 +28,17 @@ function recPerimeter(){
 let pi = 3.1415926;
 
 function cirArea(){
-
+      let r = parseFloat( document.getElementById("rad").value);
+      let op = document.getElementById("output");
+      let A = pi * (r **2);
+      op.innerHTML = "Area of circle is: " + A;
 }
 
 function cirPerimeter(){
-
+      let r = parseFloat( document.getElementById("rad").value);
+      let op = document.getElementById("output");
+      let C = 2*pi*r;
+      op.innerHTML = "Circumference of circle is: " + C;
 }
 
 /* Challenge Bonus: Complete the area and perimeter functions below for the triangle.  Guidelines,
