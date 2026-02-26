08425aa136315dd37b7a070285f6e76b8188a540
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-02-26 18:44:12 +0000

    xxx

---
+++ WDLesson4_2Practical_Soln/script.js
@@ -0,0 +1,61 @@
+// General Guideline
+/*
+function name(){
+  // Step 1: Create variables for the information from the text inputs and the output element
+
+  // Step 2: Complete the process
+  //    a) Perform any calculations necessary
+  //    b) Perform necessary decisions
+
+  // Step 3: Display the output
+
+}
+*/
+
+/* Challenge 2:  Create the function for Challenge 1 scenario */
+function area(){
+    let l = parseFloat(document.getElementById("length").value);
+    let w = parseFloat(document.getElementById("width").value);
+    let output = document.getElementById("output");
+    let message = "";
+    if (l <= 0 || w <= 0 ){
+      message = "Inappropriate measurements";
+    }else{
+      let A = l * w;
+      message = `Area of rectangle with width ${w} and length ${l} is ${A} sq.units.`;
+    }
+
+    output.innerHTML = message;
+    //output.innerHTML = `${message}`;
+}
+
+/* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
+1) Review the HTML for the UI.  Remember dropdown boxes are just input.
+2) You will need to write HTML as part of the output.  
+   For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
+function pet(){
+    let a = document.getElementById("animal").value;
+    let e = document.getElementById("emotion").value;
+    let output = document.getElementById("output");
+    let filename = "";
+    if (a == "Bear" && e == "Funny"){
+      filename = "funnyBear.jpg";
+    }
+    if (a == "Bear" && e == "Sad"){
+      filename = "sadBear.jpg";
+    }
+    if (a == "Cat" && e == "Funny"){
+      filename = "funnyCat.jpg";
+    }
+    if (a == "Cat" && e == "Sad"){
+      filename = "sadCat.jpg";
+    }
+    if (a == "Dog" && e == "Funny"){
+      filename = "funnyDog.jpg";
+    }
+    if (a == "Dog" && e == "Sad"){
+      filename = "sadDog.jpg";
+    }
+
+    output.innerHTML = `<img src="${filename}">`;
+}
---
+++ WDLesson4_3Practical_Soln/script.js
@@ -0,0 +1,43 @@
+/* Challenge 2: Create a function to ,
+       1) Calculate and display the BMI.  Look up the formula which you should have done in Challenge 1
+       2) Display BMI status as per the chart in index.html
+       3) Display the corresponding image based on the BMI status.
+*/
+/* 
+Guideline:
+1) Create the function
+2) Get the information from the UI
+3) Perform the necessary calculation.
+4) Make the appropriate decisions.  Store the appropriate BMI status.  Store the appropriate filename for the image.
+5) Display the output
+
+Use past examples, classwork and practicals to assist you in completing this practical.
+*/
+function bmi_calc(){
+       //variables
+       let h = parseFloat( document.getElementById("h").value );
+       let w = parseFloat( document.getElementById("w").value );
+       let op = document.getElementById("output");
+       let status ="";
+       let filename="";
+
+       //calculation
+       let bmi = 703 * w / (h**2);
+
+       //decisions
+       if (bmi < 18.5){
+              status = "Underweight";
+              filename = "underweight.png";
+       }else if (bmi >= 18.5 && bmi < 25){
+              status = "Healthy Weight";
+              filename = "healthyweight.png";
+       }else if (bmi >= 25 && bmi < 30){
+              status = "Overweight";
+              filename = "overweight.png";
+       }else if (bmi >= 30){
+              status = "Obese";
+              filename = "obeseweight.png";
+       }
+
+       op.innerHTML = `<img src="${filename}"><br>You are ${status}`;
+}
