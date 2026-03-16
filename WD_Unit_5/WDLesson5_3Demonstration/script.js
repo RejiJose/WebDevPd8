b125d320a51913f88659cc6597526715af6e2a09
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-03-16 17:21:58 +0000

    jj

---
+++ WD_Unit_5/WDLesson5_3Demonstration/script.js
@@ -0,0 +1,24 @@
+
+
+function generate(){
+  let build ="";
+  build += `<table border="1">
+                    <tr class="headings">
+                      <th>X</th>
+                      <th>Y</th>
+                    </tr>;`
+  let output = document.getElementById("output");
+  let startX = parseInt(document.getElementById("startX").value);
+  let endX = parseInt(document.getElementById("endX").value);
+
+  for(let x = startX; x <= endX; x++){
+    let y = x ** 2;
+    build += ` <tr class="data">
+                  <td>${x}</td>
+                  <td>${y}</td>
+              </tr>`;
+  }
+  build += `</table>`;
+
+  output.innerHTML = build;
+}
