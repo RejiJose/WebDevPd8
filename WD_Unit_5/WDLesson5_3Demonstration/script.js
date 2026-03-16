c505c43e0dfc9c7aea5ddf328b1a260fd0f60202
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-03-16 18:09:24 +0000

    ll

--- WD_Unit_5/WDLesson5_3Demonstration/script.js
+++ WD_Unit_5/WDLesson5_3Demonstration/script.js
@@ -1,15 +1,14 @@
-
-
 function generate(){
+  let output = document.getElementById("output");
+  let startX = parseInt(document.getElementById("startX").value);
+  let endX = parseInt(document.getElementById("endX").value);
+  
   let build ="";
   build += `<table border="1">
                     <tr class="headings">
                       <th>X</th>
                       <th>Y</th>
                     </tr>`;
-  let output = document.getElementById("output");
-  let startX = parseInt(document.getElementById("startX").value);
-  let endX = parseInt(document.getElementById("endX").value);
 
   for(let x = startX; x <= endX; x++){
     let y = x ** 2;
