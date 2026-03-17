cc9c2988750d5cadc0bcf269c5d8949e75d8e580
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-03-17 12:11:40 +0000

    cc

--- WD_Unit_5/WDLesson5_3Practical_Soln/script.js
+++ WD_Unit_5/WDLesson5_3Practical_Soln/script.js
@@ -1,13 +1,13 @@
 /* Challenge: Complete the Compound Interest Calculator
         1) Retrieve and store all the information in appropriate values. 
-           Note: Typically user enter rates as percentages.  Divide the value retrieved by 100.
-                 Remember principals and rates have decimals.
+           Note: Typically user enter rates as percentages. Divide the value retrieved by 100.
+                 Remember principal and rate have decimals.
         2) Create a build variable to start the table
-        3) Establish the for loop to iterate over the number of months in the investment
-        4) Calculate the balance using compound interest for each year. Hint: Loop variable
+        3) Establish the for loop to iterate over the number of years in the investment
+        4) Calculate the balance using compound interest for each year. (Hint: Loop variable)
         5) Build each row for the balance after each year
-           Note: When interpolating the balance, use a.toFixed(2) to adjust the amounts to two decimal places.  
-        6) After the for loop complete building the table and display it
+           Note: When interpolating the balance, use toFixed(2) to adjust the amounts to two decimal places.  
+        6) After the for loop, complete building the table and display it
 */
 
 function balance(){
@@ -15,12 +15,12 @@ function balance(){
 
 }
 
-/* Challenge Bonus: Allow the user to enter n.  This will require you to modify,
+/* Challenge Bonus: Allow the user to enter a value for "n". This will require you to modify as follows:
         1) Retrieve the value of n from the user.
         2) Modify the formula to incorporate the value of n
-        3) Adjust the heading reflect the compound length.  Below are some typical lengths
-              a. n = 1 then the interest is compounded yearly
-              b. n = 12 then the interest is compounded monthly
-              c. n = 3 then the interest is compounded quarterly
-              d. n = 2 then the interest is compounded bi-yearly
-*/ 
+        3) Adjust the heading to reflect the compounding period. Below are some typical n values.
+              a. n = 1 when interest is compounded yearly
+              b. n = 4 when interest is compounded quarterly
+              c. n = 12 when interest is compounded monthly
+              
+*/
