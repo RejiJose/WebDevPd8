e459a985bad4b9634874d3dc24d74b1fc2dd63cb
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-03-17 11:59:20 +0000

    kk

--- WD_Unit_5/WDLesson5_3Demonstration/script.js
+++ WD_Unit_5/WDLesson5_3Demonstration/script.js
@@ -12,11 +12,12 @@ function generate(){
 
   for(let x = startX; x <= endX; x++){
     let y = x ** 2;
-    build += ` <tr class="data">
+    build += `<tr class="data">
                   <td>${x}</td>
                   <td>${y}</td>
               </tr>`;
   }
+  
   build += `</table>`;
 
   output.innerHTML = build;
