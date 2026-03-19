2e581f9b6e5ff4c05eb262371070230299bb1330
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-03-19 12:29:11 +0000

    aaa

--- WD_Unit_6/WDLesson6_1bDemonstration/script.js
+++ WD_Unit_6/WDLesson6_1bDemonstration/script.js
@@ -1,7 +1,10 @@
 function init(){
   let output = document.getElementById("output");
+  
   let names = ["Captain America","Colossus","Gambit","Hulk","Phoenix","Psylocke","Rogue","Spiderman","Storm","Thor","Venom","Wolverine"];
+  
   let heroes = ["captainamerica.gif","colossus.gif","gambit.gif","hulk.gif","phoenix.gif","psylocke.gif","rogue.gif","spiderman.gif","storm.gif","thor.gif","venom.gif","wolverine.gif"];
+  
   let build = "";
 
   for(let i = 0; i < heroes.length; i++){
@@ -10,7 +13,6 @@ function init(){
     build += `   <img src="heroes/${heroes[i]}">`;
     build += `</div>`;
   }
- 
- 
+  
   output.innerHTML = build;
 }
