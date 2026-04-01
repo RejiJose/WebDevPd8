a8aed5590f5455a99d86e32054a26c4d2a177823
Reji Joseph <rjoseph82@schools.nyc.gov>
2026-04-01 13:03:01 +0000

    kk

--- WD_Unit_6/WDLesson6_2Practical_Soln/script.js
+++ WD_Unit_6/WDLesson6_2Practical_Soln/script.js
@@ -7,20 +7,34 @@ function BHS(){
   let output = document.getElementById("output");
 
   //Challenge 1: Create and display a card of the information contained in the JSON variable school
-
+  let build = "";
+  build += `<div class="card">
+                <h2>${school.name}</h2>
+                <img src="${school.image}">
+                <p>${school.address}</p>
+            </div>`;
+  output.innerHTML = build;
 }
 function artist(){
   //Challenge 2: Fill the JSON below with the specified information for your favorite artist
   let artist = {
-    "name":"",
-    "image":"",
-    "album":"",
-    "url":""
+    "name":"Taylor Swift",
+    "image":"https://publish.purewow.net/wp-content/uploads/sites/2/2024/10/taylor-swift-hub.jpg",
+    "album":"The Life of a Showgirl",
+    "url":"https://www.taylorswift.com"
   };
   let output = document.getElementById("output");
 
   //Challenge 3: Build a card for the information in the JSON. Make the image a hyperlink to the url provided.
-
+  let build = "";
+  build += `<div class="card">
+                <h2>${artist.name}</h2>
+                <p>${artist.album}</p>
+                <a href="${artist.url}" target="_blank">
+                  <img src="${artist.image}">
+                </a>                
+            </div>`;
+  output.innerHTML = build;
 }
 
 
---
+++ WD_Unit_6/WDLesson6_3Demonstration/script.js
@@ -0,0 +1,30 @@
+let teachers = [
+                { "name":"Benn", "course":"Programming Technologies", "image":"programmingtech.jpg"},
+                { "name":"Joseph", "course":"Web Development", "image":"webdev.png" },
+                { "name":"Porchetta", "course":"Intro to Computer Science", "image":"introcs.jpg" },
+                { "name":"Castro", "course":"CS Career Development", "image":"cscareerdev.jpg" }
+              ];
+
+
+function example1(){
+  console.log(teachers[0]);
+  console.log(teachers[1].course);
+  console.log(teachers[2].name);
+  console.log(teachers[3].image);
+}
+
+function example2(){
+  let output = document.getElementById("output");
+  let build = "";
+
+  for(let i = 0; i < teachers.length; i++){
+    let teacher = teachers[i];
+    build += `<div class="card">
+                <h3>${teachers[i].course}</h3>
+                <img src="${teachers[i].image}">
+                <h3>${teachers[i].name}</h3>
+              </div>`;
+  }
+  
+  output.innerHTML = build;
+}
